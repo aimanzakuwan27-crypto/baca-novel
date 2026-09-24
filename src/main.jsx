a2765f77b1bef465ded23bb0cffcd57517d0cad9
aimanzakuwan27-crypto <aimanzakuwan27@gmail.com>
2026-09-24 21:01:11 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import BacaNovelApp from "./baca-novel.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
